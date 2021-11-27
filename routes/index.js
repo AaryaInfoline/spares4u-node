10d25f19ca4fb66d7c3154d2cccf30900fe53957
Harikrushna Patel <github@haripatel.co.in>
2021-11-27 16:25:31 +0530

    new

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-
-app.use(require('../middlewares/middleware').global);
-app.use('/',express.static('../public/dist'));
-app.use('/data',express.static('../public/storage'));
-app.use('/api',require('../routes/mobile.routes'));
-app.use('/web',require('../routes/web.routes'));
+const middleware = require('../middlewares/middleware');
+app.use(middleware.global);
+app.use('/', express.static('../public/dist'));
+app.use('/data', express.static('../public/storage'));
+app.use('/api', require('../routes/mobile.routes'));
+app.use('/web', require('../routes/web.routes'));
 module.exports.app = app;
